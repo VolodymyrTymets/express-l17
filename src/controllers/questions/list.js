761578e0592053486f0a843f7691507d2ce40fbd
volodymyr <volodymyrtymets@gmail.com>
2018-04-24 16:44:02 +0300

    add skip, filters, search, sort

--- src/controllers/questions/list.js
+++ src/controllers/questions/list.js
@@ -1,6 +1,20 @@
+const _ = require('lodash');
+
 const list = ({ Question }, { config }) => async (req, res, next) => {
+  let { limit, skip, search } = req.query;
+  skip = skip ? parseInt(skip, 10) : 0;
+  limit = limit ? parseInt(limit, 10) : 100;
+
   try {
-    const questions = await Question.find({});
+    const query = {};
+    if (search) {
+      _.extend(query, { title: new RegExp(`${search}`, 'i') })
+    }
+    const questions = await Question.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort({ _id: -1 });
+
     res.status(200).send({ questions });
   } catch (error) {
     next(error);
